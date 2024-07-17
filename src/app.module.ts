a1abe85927c005e11744ae43db502a6554d43981
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2024-07-17 23:29:35 +0200

    linter app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { dataSourceOptions } from "./config/typeormconfig";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './config/typeormconfig';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forRoot(dataSourceOptions)],
